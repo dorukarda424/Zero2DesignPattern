b88ea01f7e56faaee54c9e829451cdd8a47cde75
Doruk Arda Ceviz <111368141+dorukarda424@users.noreply.github.com>
2023-08-27 13:36:16 +0000

    odev1

--- odev1.py
+++ odev1.py
@@ -1,15 +1,16 @@
+
 num_list = [100, 1, 43, 5, 12, 32, 54, 20, 12, 3]
 temp_list = num_list.copy()
 sorted_list = []
 count = 0
 while count < len(num_list):
-    min=0
+    min_num=0
     for j in range(0,len(temp_list)):  
-        if min == 0:
-            min = temp_list[j]
-        elif temp_list[j] < min:
-            min = temp_list[j]
-    sorted_list.append(min)
-    temp_list.pop(temp_list.index(min))
+        if min_num == 0:
+            min_num = temp_list[j]
+        elif temp_list[j] < min_num:
+            min_num = temp_list[j]
+    sorted_list.append(min_num)
+    temp_list.pop(temp_list.index(min_num))
     count+=1
 print(sorted_list)
